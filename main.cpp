6b0332f57d1bea9b9acb1793f0e50cb6b4062c8a
raymanhuang <122694075+raymanhuang@users.noreply.github.com>
2023-08-12 03:52:14 -0700

    testing

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     Solution s;
     cout << s.isPalindrome("A man, a plan, a canal, Panama!");
-
+    std::string test = "testing";
     return 0;
 }
